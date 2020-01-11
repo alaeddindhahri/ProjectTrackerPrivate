029a8e3ebc716cef5a36d81eb918e483e20e7280
Haroun Moujahed <haroun.moujahed.yu@gmail.com>
2020-01-11 19:46:18 +0100

    changed the landingPage and cnxmodal

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
 import "./App.css";
-import {BrowserRouter,Route, Switch} from 'react-router-dom';
-import jwt_decode from 'jwt-decode';
-import setAuthAdminToken from './utils/setAuthAdminToken';
-import {setCurrentAdmin, logoutAdmin} from './actions/authAdminAction';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthAdminToken from "./utils/setAuthAdminToken";
+import { setCurrentAdmin, logoutAdmin } from "./actions/authAdminAction";
 
-import {Provider} from 'react-redux';
-import Store from './store/index';
+import { Provider } from "react-redux";
+import Store from "./store/index";
 
-import ZookiLP from "./Components/zookiLP/zookiLP";
-import AdminDashboard from './Components/adminDashboard/Index'
-import AdminLogin from './Components/adminLogin/index';
+import LandingPage from "./Components/LandingPage/LandingPage";
+import AdminDashboard from "./Components/adminDashboard/Index";
+import AdminLogin from "./Components/adminLogin/index";
 import store from "./store/index";
 
 //check for token
-if(localStorage.jwtToken){
+if (localStorage.jwtToken) {
   //set auth token header auth
   setAuthAdminToken(localStorage.jwtToken);
   //decode token and get admin info and expiration
@@ -22,8 +22,8 @@ if(localStorage.jwtToken){
   //set admin and isAuthenticated
   store.dispatch(setCurrentAdmin(decoded));
   //check for expired token
-  const currentTime = Date.now()/1000;
-  if(decoded.exp<currentTime){
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
     //logout admin
     store.dispatch(logoutAdmin());
     //TODO clear admin account
@@ -35,14 +35,14 @@ function App() {
   return (
     <Provider store={Store}>
       <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={ZookiLP}/>
-          <Route exact path="/adminlogin" component={AdminLogin}/>
-          <Route path="/admindashboard" component={AdminDashboard}/>
-        </Switch>
-      </div>
-    </BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/adminlogin" component={AdminLogin} />
+            <Route path="/admindashboard" component={AdminDashboard} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </Provider>
   );
 }
--- client/src/Components/CnxModal/CnxModal.jsx
+++ client/src/Components/CnxModal/CnxModal.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import "../LandingPage/LandingPage.css";
+import axios from "axios";
 
 class CnxModal extends React.Component {
+  state = {
+    userType: "",
+    newInstructor: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      username: "",
+      password: "",
+      phoneNumber: "",
+      birthDate: "",
+      isActive: false
+    }
+  };
+  addInstructor = newInstructor => {
+    axios
+      .post("/api/instructors/registerinstructor", newInstructor)
+      .then(console.log("Instructor added Successfully"));
+  };
+
+  handleChange = e => {
+    this.setState({
+      newInstructor: {
+        ...this.state.newInstructor,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
   render() {
     return (
       // <Modal
@@ -12,7 +40,8 @@ class CnxModal extends React.Component {
       // >
       <Modal isOpen={this.props.modal} toggle={this.props.toggle}>
         <ModalHeader toggle={this.toggle}>{this.props.type}</ModalHeader>
-        <ModalBody>
+        <ModalBody className="formArea">
+          {/* <div className="formArea"> */}
           <img
             src="https://dcassetcdn.com/design_img/1559024/551167/551167_7840631_1559024_911ff84c_image.png"
             width="150"
@@ -22,25 +51,113 @@ class CnxModal extends React.Component {
           />
           <h3>Project Tracker</h3>
           <div className="userType">
-            <button type="button" class="btn btn-secondary">
+            <button
+              type="button"
+              className="btn btn-info"
+              onClick={() => this.setState({ userType: "Student" })}
+            >
               Student
             </button>
-            <button type="button" class="btn btn-secondary">
+            <button
+              type="button"
+              className="btn btn-info"
+              onClick={() => this.setState({ userType: "Instructor" })}
+            >
               Instructor
             </button>
           </div>
-          <p>Email Address: </p>
-          <input type="email" class="form-control" id="emailArea" />
-          <p>Password: </p>
-          <div className="passwordZone">
-            <input type="password" class="form-control " id="passwordArea" />
-            <a href="/" className="forgotPassword">
-              <p>Forgot Password?</p>
-            </a>
-          </div>
+          <h5 id="welcomeMsg">
+            Welcome <span>{this.state.userType}</span>
+          </h5>
+          {this.props.type === "Login" ? (
+            <>
+              <input
+                type="email"
+                className="form-control"
+                id="emailArea"
+                placeholder="Email..."
+              />
+              <div className="passwordZone">
+                <input
+                  type="password"
+                  className="form-control "
+                  id="passwordArea"
+                  placeholder="Password..."
+                />
+                <a href="/" className="forgotPassword">
+                  <p>Forgot Password?</p>
+                </a>
+              </div>
+            </>
+          ) : (
+            <></>
+          )}
+          {this.props.type === "Sign Up" ? (
+            <>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="First Name..."
+                name="firstName"
+                onChange={this.handleChange}
+              />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Last Name..."
+                name="lastName"
+                onChange={this.handleChange}
+              />
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email..."
+                name="email"
+                onChange={this.handleChange}
+              />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="UserName..."
+                name="username"
+                onChange={this.handleChange}
+              />
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                name="password"
+                onChange={this.handleChange}
+              />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="PhoneNumber..."
+                name="phoneNumber"
+                onChange={this.handleChange}
+              />
+              <p>Birth date: </p>
+              <input
+                className="form-control"
+                type="date"
+                id="example-date-input"
+                name="birthDate"
+                onChange={this.handleChange}
+              ></input>
+            </>
+          ) : (
+            <></>
+          )}
+          {/* </div> */}
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={this.props.toggle}>
+          <Button
+            color="primary"
+            onClick={() => {
+              this.props.toggle();
+              this.addInstructor(this.state.newInstructor);
+            }}
+          >
             {this.props.type}
           </Button>{" "}
           <Button color="secondary" onClick={this.props.toggle}>
