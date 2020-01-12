056cff1a6b2c13357db87cceda9b1f831e49306b
sikipon123 <zouitinef@gmail.com>
2020-01-12 12:29:07 +0100

    student add css

---
+++ client/src/Components/studentDashboard/Index.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import Navbar from './navbar/navbar';
+import Sidebar from './sidebar/sidebar';
+import DashboardContent from './dashboardContent/index';
+import {setProjects} from '../../actions/projectsStudentAction';
+
+
+
+ class index extends Component {
+     state={
+         section:"Statistics",
+     }
+     handleRenderedSection=(section,e)=>{
+         e.preventDefault();
+         this.setState({
+             section:section
+         })
+     }
+    componentDidMount(){
+        if(!this.props.authStudent.isAuthenticated){
+            return this.props.history.push('/studentlogin');
+        }
+        if(this.props.authStudent.students.length!==0){
+            this.setState({
+                section:"Statistics"
+            })
+        }
+        this.props.setProjects();
+    }
+    render() {
+        return (
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col top-navbar">
+                        <Navbar handleRenderedSection={this.handleRenderedSection}/>
+                    </div>
+                </div>
+                <div className="row sidebar-content">
+                    <div className="col-lg-2 sidebar">
+                        <Sidebar handleRenderedSection={this.handleRenderedSection}/>
+                    </div>
+                    <div className="col-lg-10 dashboard-content">
+                        <DashboardContent section={this.state.section}/>
+                    </div>
+                </div>
+                <div className="row sticky-footer">
+                    <div className="copyright text-center my-auto">
+                        <span>Copyright © Project Tracker 2020</span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+index.propTypes = {
+    authStudent: PropTypes.object.isRequired,
+    errors:PropTypes.object.isRequired
+
+}
+const mapStateToProps=state=>({
+    authStudent: state.authStudent,
+    errors: state.errors
+})
+export default connect(mapStateToProps,{setProjects})(index);
