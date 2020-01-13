9c7daf01aa74dd05655592f5284a7ea213fc4ed8
Haroun Moujahed <haroun.moujahed.yu@gmail.com>
2020-01-13 09:39:16 +0100

    started the instructor autheutification developement

--- config/passport.js
+++ config/passport.js
@@ -1,22 +1,26 @@
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const mongoose = require('mongoose');
-const Admin = mongoose.model('admin');
-const keys = require('../config/keys');
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const Admin = mongoose.model("admin");
+// const Instructor = mongoose.model("instructor");
+const keys = require("../config/keys");
 
-const opts={};
+const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secretOrKey;
 
-module.exports = (passport) =>{
-    passport.use(new JwtStrategy(opts,(jwt_payload,done)=>{
-        Admin.findById(jwt_payload.id)
-            .then(admin=>{
-                if(admin){
-                    return done(null,admin);
-                }
-                return done(null,false);
-            })
-            .catch(err=>console.log(err));
-    }));
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      console.log(jwt_payload);
+      //   Admin.findById(jwt_payload.id)
+      //     .then(admin => {
+      //       if (admin) {
+      //         return done(null, admin);
+      //       }
+      //       return done(null, false);
+      //     })
+      //     .catch(err => console.log(err));
+    })
+  );
 };
